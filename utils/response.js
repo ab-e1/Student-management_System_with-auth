abd394b3d8b3b07ba647b3e0512c7198f49e2d7e
ab-e1 <abelghy17@gmail.com>
2026-08-19 00:23:09 +0300

    added a response helper for success and failure as well as public infos about users

---
+++ utils/response.js
@@ -0,0 +1,19 @@
+const success = (res, data, statusCode = 200) => {
+  res.status(statusCode).json({ ok: true, data: data });
+};
+
+const failure = (res, error, statusCode = 400) => {
+  res.status(statusCode).json({ ok: false, error: error });
+};
+
+const visibleInfo = (data) => {
+  return {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    role: data.role,
+    registerdAt: data.registerdAt,
+  };
+};
+
+module.exports = { success, failure, visibleInfo };
