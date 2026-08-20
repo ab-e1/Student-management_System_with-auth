017b6e068c5f7cb86edf184ca5a3a42212cdbfa1
ab-e1 <abelghy17@gmail.com>
2026-08-21 00:01:55 +0300

    added the jeson web token handeler logic for creating singtoken and verifying so we can use them again

---
+++ utils/generateToken.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+const { jwtSecret, jwtExpiresIn } = require("../config/index.js");
+
+const signToken = (payload) => {
+  return jwt.sign(payload, jwtSecret, {
+    expiresIn: jwtExpiresIn,
+  });
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, jwtSecret);
+};
+
+module.exports = { signToken, verifyToken };
