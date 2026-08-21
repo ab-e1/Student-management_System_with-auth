eb8a4592bc14ca4909e280a50978c0169d772c79
ab-e1 <abelghy17@gmail.com>
2026-08-21 21:47:57 +0300

    fixed route callin services directly error , chnaged to dontroller

--- route/userRoutes.js
+++ route/userRoutes.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
-const userServices = require("../services/userServices.js");
+const userController = require("../controller/userController.js");
 const auth = require("../middleware/authMiddleware.js");
 const roleCheck = require("../middleware/roleMiddleware.js");
 
 const router = Router();
 
-router.get("/", auth, roleCheck("admin"), userServices.getAllUsers);
+router.get("/", auth, roleCheck("admin"), userController.getAllUsers);
 
 module.exports = router;
