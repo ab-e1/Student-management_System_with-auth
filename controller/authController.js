a8532058fe42e1b958f83c40bd8dcdd487a6113f
ab-e1 <abelghy17@gmail.com>
2026-08-21 11:59:35 +0300

    added teh auth controller

---
+++ controller/authController.js
@@ -0,0 +1,20 @@
+const authServices = require("../services/authServices.js");
+const { success, failure } = require("../utils/response.js");
+
+const register = (req, res) => {
+  const result = authServices.register(req.body);
+  if (!result.ok) {
+    return failure(res, result.error, 400);
+  }
+  return success(res, { ...result.data, token: result.token });
+};
+
+const login = (req, res) => {
+  const result = authServices.login(req.body);
+  if (!result.ok) {
+    return failure(res, result.error, 400);
+  }
+  return success(res, { ...result.data, token: result.token });
+};
+
+module.exports = { register, login };
