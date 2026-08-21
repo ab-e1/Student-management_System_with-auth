63d403c76e7b80d6226f0a1e8bd2c4db1fa803fb
ab-e1 <abelghy17@gmail.com>
2026-08-21 13:01:21 +0300

    added teh app.js logic , and configured the server so the app listents on the given port

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const userRoutes = require("./route/userRoutes.js");
+const studentRoutes = require("./route/studentRoutes.js");
+const authRoute = require("./route/authRoute.js");
+const erroHandler = require("./middleware/errorHandeler.js");
+const logger = require("./middleware/logger.js");
+const errorHandeler = require("./middleware/errorHandeler.js");
+
+const app = express();
+app.use(express.json());
+
+app.use(logger);
+
+app.use("/api/auth", authRoute);
+app.use("/api/students", studentRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(errorHandeler);
+
+module.exports = app;
