00098b2ea0079300a9534a38d4d138d3446e7e9d
ab-e1 <abelghy17@gmail.com>
2026-08-21 16:50:20 +0300

    added the logout and me enpotins in the router

--- route/authRoute.js
+++ route/authRoute.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const authController = require("../controller/authController.js");
 const { validateUser } = require("../middleware/validation.js");
+const auth = require("../middleware/authMiddleware.js");
 
 const router = Router();
 
 router.post("/register", validateUser, authController.register);
 router.post("/login", authController.login);
+router.post("/logout", auth, authController.logout);
+router.get("/me", auth, authController.me);
 
 module.exports = router;
